030ed7a0bde08207e0b883a204ed2c9189f67d7e
Geet Choubey <geetchoubey@gmail.com>
2019-05-31 13:46:33 +0530

    Hardcoding file name

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -3,6 +3,6 @@ resource "aws_lambda_function" "demo_function" {
   handler = "index.handler"
   role = "${var.IAM_LAMBDA_ARN}"
   filename = "${var.API_DIR}"
-  source_code_hash = "${base64sha256(file("${var.API_DIR}"))}"
+  source_code_hash = "${base64sha256(file("terraform_demo-api.zip"))}"
   runtime = "nodejs8.10"
 }
