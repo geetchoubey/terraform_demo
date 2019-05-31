af1f12097530e445c6fccb1086f1c08677af72dd
Geet Choubey <geetchoubey@gmail.com>
2019-05-31 14:33:00 +0530

    Local file changes

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "demo_function" {
   handler = "index.handler"
   role = "${var.IAM_LAMBDA_ARN}"
   filename = "${var.API_DIR}"
-  source_code_hash = "${base64sha256(data.local_file.dist_file)}"
+  source_code_hash = "${base64sha256(data.local_file.dist_file.content)}"
   runtime = "nodejs8.10"
 }
 
